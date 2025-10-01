01f69eb17c7f09895b585a4ad024335572cc9fd6
Richard Brearton <richardbrearton@gmail.com>
2025-10-01 23:07:02 +0100

    Added some absolute basics to the Actor

--- src/Flop.Core/Actor.cs
+++ src/Flop.Core/Actor.cs
@@ -1,3 +1,23 @@
-﻿namespace Flop.Core;
+﻿using System.Numerics;
+using Flop.Core.Geometry;
 
-public abstract class Actor { }
+namespace Flop.Core;
+
+public abstract class Actor
+{
+    /// <summary>
+    /// The actor's geometry rig.
+    /// This contains all the rigid bodies (geometry components) that make up the actor.
+    /// </summary>
+    public abstract IGeometryRig GeometryRig { get; }
+
+    /// <summary>
+    /// The actor's absolute position in the game world.
+    /// </summary>
+    public abstract Vector3 Position { get; }
+
+    /// <summary>
+    /// The actor's absolute rotation in the game world.
+    /// </summary>
+    public abstract Quaternion Rotation { get; }
+}
