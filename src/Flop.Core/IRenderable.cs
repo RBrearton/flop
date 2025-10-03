a68cf9a37a5e240fc5bcc3602ae1d689008e5ea8
Richard Brearton <richardbrearton@gmail.com>
2025-10-03 17:52:56 +0100

    Updated IRenderable to make it more suitable

--- src/Flop.Core/IRenderable.cs
+++ src/Flop.Core/IRenderable.cs
@@ -5,4 +5,10 @@ namespace Flop.Core;
 /// <summary>
 /// An interface for objects that can be rendered.
 /// </summary>
-public interface IRenderable : IGeometryRig, IHasPlacement { }
+public interface IRenderable : IHasPlacement
+{
+    /// <summary>
+    /// The geometry rig that we'll render.
+    /// </summary>
+    IGeometryRig GeometryRig { get; }
+}
