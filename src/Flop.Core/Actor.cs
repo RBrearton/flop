a023c003bf2ebaba9c03107d63e5ca29863de6b3
Richard Brearton <richardbrearton@gmail.com>
2025-10-04 13:11:49 +0100

    Updated and simplified Actor class

--- src/Flop.Core/Actor.cs
+++ src/Flop.Core/Actor.cs
@@ -1,35 +1,14 @@
 ﻿using System.Numerics;
-using Flop.Core.Geometry;
-using Flop.Core.Geometry.Primitives;
 
 namespace Flop.Core;
 
-public abstract class Actor : IRenderable, IDescribable
+public abstract class Actor(Identity identity, Vector3 position, Quaternion rotation)
+    : Entity(identity, position, rotation),
+        IDescribable
 {
-    /// <summary>
-    /// The actor's geometry rig.
-    /// This contains all the rigid bodies (geometry components) that make up the actor.
-    /// </summary>
-    public abstract IGeometryRig GeometryRig { get; }
-
     /// <summary>
     /// The description of the actor.
     /// This will be used by the game's UI when necessary to provide things like tooltips etc.
     /// </summary>
     public abstract string Description { get; }
-
-    /// <summary>
-    /// The actor's bounding box in the game world.
-    /// </summary>
-    public Box BoundingBox => GeometryRig.BoundingBox;
-
-    /// <summary>
-    /// The actor's absolute position in the game world.
-    /// </summary>
-    public Vector3 Position { get; }
-
-    /// <summary>
-    /// The actor's absolute rotation in the game world.
-    /// </summary>
-    public Quaternion Rotation { get; }
 }
